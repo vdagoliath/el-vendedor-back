25d09a595c50c5fba6ee0b0401d0cd949d0e70f1
Víctor Daniel Arteaga Goliath <vdagoliath@gmail.com>
2026-05-23 08:14:03 -0400

    flexibilizando contrasena de duenos

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -41,14 +41,6 @@ class AppServiceProvider extends ServiceProvider
             app()->isProduction(),
         );
 
-        Password::defaults(fn (): ?Password => app()->isProduction()
-            ? Password::min(12)
-                ->mixedCase()
-                ->letters()
-                ->numbers()
-                ->symbols()
-                ->uncompromised()
-            : null,
-        );
+        Password::defaults(fn (): Password => Password::min(8));
     }
 }
