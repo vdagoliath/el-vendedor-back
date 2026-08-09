ee27acc8edb6b75b50576e4a9d85a093385305bb
Víctor Daniel Arteaga Goliath <vdagoliath@gmail.com>
2026-08-09 16:56:29 -0400

    cors

--- config/cors.php
+++ config/cors.php
@@ -1,15 +1,23 @@
 <?php
 
+$allowedOrigins = array_values(array_filter(array_map('trim', explode(',', env('CORS_ALLOWED_ORIGINS', '')))));
+
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [],
+    'allowed_origins' => $allowedOrigins,
 
     'allowed_origins_patterns' => [
         '#^http://localhost(:\d+)?$#',
+        '#^https://localhost(:\d+)?$#',
         '#^http://127\.0\.0\.1(:\d+)?$#',
+        '#^capacitor://localhost$#',
+        '#^ionic://localhost$#',
+        '#^http://10\.\d{1,3}\.\d{1,3}\.\d{1,3}(:\d+)?$#',
+        '#^http://172\.(1[6-9]|2\d|3[0-1])\.\d{1,3}\.\d{1,3}(:\d+)?$#',
+        '#^http://192\.168\.\d{1,3}\.\d{1,3}(:\d+)?$#',
     ],
 
     'allowed_headers' => ['*'],
